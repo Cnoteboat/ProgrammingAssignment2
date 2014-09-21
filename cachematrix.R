98eda1a0bab7802e29b01bd3d580ae35813d05f5
Cnoteboat <charlie.boatwright@gmail.com>
2014-09-21 08:37:39 -0700

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,26 +1,26 @@
-#still need to make comments here
+#This is my solution for programming Assignment 2
 
-makeCacheMatrix<- function(x= matrix()){
+makeCacheMatrix<- function(x= matrix()){ #Here I define that we must be working with Matrices
     s <- NULL
-    set <- function(y){
+    set <- function(y){  #Setting the Value of the Matrix
       x<<- y
       s<<- NULL
     }
-    get<- function() x
-    setinverse<- function(solve) s <<- solve
-    getinverse<- function() s
-    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+    get<- function() x #Getting the Value of the Matrix
+    setinverse<- function(solve) s <<- solve #Setting the value of the Inverse with the solve() function
+    getinverse<- function() s #getting the Value of the inverse Matrix
+    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse) 
 }
 
 cacheSolve<- function(x, ...) {
-  s <- x$getinverse()
+  s <- x$getinverse() #get solved inverse Matrix
   if(!is.null(s)) {
-    message("getting cached data")
+    message("getting cached data") #check for cached solution where it returns "getting cached data" if the solution is cached
     return(s)
   }
-  data <- x$get()
-  s <- solve(data, ...)
-  x$setinverse(s)
+  data <- x$get() #otherwise get the value of the matrix x
+  s <- solve(data, ...) #and return the solution of matrix x
+  x$setinverse(s) #
   s
 }
 
